7e9f94992dc832c6d30a28d2b5e9d6af3fc6a408
Siddesh <siddesh976@gmail.com>
2019-10-17 16:47:36 +0530

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import firestore from 'firebase/firestore'
 // const settings = {timestampsInSnapshots: true};
 
 const config = {
-    apiKey: "AIzaSyBNZUGIdFEzFXjQlLrebfQklFkT1Oa4n3A",
-    authDomain: "food-b2523.firebaseapp.com",
-    databaseURL: "https://food-b2523.firebaseio.com",
-    projectId: "food-b2523",
-    storageBucket: "food-b2523.appspot.com",
-    messagingSenderId: "201802274332",
-    appId: "1:201802274332:web:2d81176601dd441e66b1d9",
-    measurementId: "G-Z9FVE0PP6J"
+    apiKey: "**********",
+    authDomain: "**********",
+    databaseURL: "**********",
+    projectId: "**********",
+    storageBucket: "**********",
+    messagingSenderId: "**********",
+    appId: "**********",
+    measurementId: "**********"
 };
 firebase.initializeApp(config);
 
